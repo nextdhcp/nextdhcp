1e19dd6fb018f30b795955fbe5722b8a8ad1a719
Patrick Pacher <p.pacher@cybertrap.com>
2019-09-04 20:07:39 +0200

    core/events: added support for lease create/expired events

---
+++ core/events/events.go
@@ -0,0 +1,52 @@
+package events
+
+import (
+	"log"
+	"runtime/debug"
+
+	"github.com/caddyserver/caddy"
+	"github.com/nextdhcp/nextdhcp/core/lease"
+)
+
+const (
+	// EventLeaseExpired is emitted when a client address lease expired
+	EventLeaseExpired caddy.EventName = "lease-expired"
+
+	// EventLeaseCreated is emitted when an address has been bound
+	// to a client
+	EventLeaseCreated = "lease-created"
+)
+
+type (
+	// LeaseEventHook is the function type that can receive lease-based events
+	LeaseEventHook func(event caddy.EventName, l *lease.Lease) error
+)
+
+var (
+	validLeaseEvents = map[caddy.EventName]struct{}{
+		EventLeaseCreated: struct{}{},
+		EventLeaseExpired: struct{}{},
+	}
+)
+
+// EmitLeaseEvent emits a lease-based event
+func EmitLeaseEvent(event caddy.EventName, l *lease.Lease) {
+	if _, ok := validLeaseEvents[event]; !ok {
+		log.Println("invalid lease event type")
+		log.Println(debug.Stack())
+		return
+	}
+
+	caddy.EmitEvent(event, l)
+}
+
+// RegisterLeaseEventHook registers a new lease event hook
+func RegisterLeaseEventHook(event caddy.EventName, hook LeaseEventHook) {
+	if _, ok := validLeaseEvents[event]; !ok {
+		panic("invalid lease event name")
+	}
+
+	caddy.RegisterEventHook(string(event), func(e caddy.EventName, value interface{}) error {
+		return hook(event, value.(*lease.Lease))
+	})
+}
