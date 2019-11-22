cf305527c08822fb4f6cd5fcc0f6828fa35d8665
Patrick Pacher <patrick.pacher@gmail.com>
2019-11-22 07:55:17 +0100

    Split up mqttPlugin setup function

--- plugin/mqtt/setup.go
+++ plugin/mqtt/setup.go
@@ -29,16 +29,6 @@ func setupMqtt(c *caddy.Controller) error {
 		cfg := &mqttConfig{}
 		useExisting := false
 
-		setConn := func() error {
-			if useExisting {
-				return c.SyntaxErr("either configure a new connection or \"use\" and existing one")
-			}
-
-			cfg.conn = &mqttConnConfig{}
-
-			return nil
-		}
-
 		cond, err := matcher.SetupMatcherRemainingArgs(c)
 		if err != nil {
 			return err
@@ -49,35 +39,12 @@ func setupMqtt(c *caddy.Controller) error {
 			switch c.Val() {
 			case "name", "broker", "user", "password",
 				"clean-session", "qos":
-				action := c.Val()
-				if err := setConn(); err != nil {
+				if useExisting {
+					return c.SyntaxErr("either configure a new connection or \"use\" and existing one")
+				}
+
+				if err := parseConnectionSettings(cfg, c); err != nil {
 					return err
-				}
-
-				if action == "clean-session" {
-					cfg.conn.cleanSession = true
-					break
-				}
-
-				if !c.NextArg() {
-					return c.ArgErr()
-				}
-
-				switch action {
-				case "name":
-					cfg.name = c.Val()
-				case "broker":
-					cfg.conn.broker = append([]string{c.Val()}, c.RemainingArgs()...)
-				case "user":
-					cfg.conn.user = c.Val()
-				case "password":
-					cfg.conn.password = c.Val()
-				case "qos":
-					i, err := strconv.Atoi(c.Val())
-					if err != nil || i < 0 || i > 2 {
-						return c.SyntaxErr("expected a number between 0 and 2")
-					}
-					cfg.conn.qos = i
 				}
 
 			case "use":
@@ -96,22 +63,14 @@ func setupMqtt(c *caddy.Controller) error {
 					return c.ArgErr()
 				}
 
-				t := c.Val()
-				cfg.topic = func(ctx context.Context, req, res *dhcpv4.DHCPv4) (string, error) {
-					rep := replacer.NewReplacer(ctx, req)
-					return rep.Replace(t), nil
-				}
+				cfg.topic = getStringFactory(c.Val())
 
 			case "payload", "body":
 				if !c.NextArg() {
 					return c.ArgErr()
 				}
 
-				t := c.Val()
-				cfg.payload = func(ctx context.Context, req, res *dhcpv4.DHCPv4) (string, error) {
-					rep := replacer.NewReplacer(ctx, req)
-					return rep.Replace(t), nil
-				}
+				cfg.payload = getStringFactory(c.Val())
 
 			case "payload-from":
 				//
@@ -127,17 +86,7 @@ func setupMqtt(c *caddy.Controller) error {
 					return c.ArgErr()
 				}
 
-				cfg.payload = func(ctx context.Context, req, res *dhcpv4.DHCPv4) (string, error) {
-					args := make([]string, len(cmd))
-					rep := replacer.NewReplacer(ctx, req)
-
-					for i, c := range cmd {
-						args[i] = rep.Replace(c)
-					}
-
-					output, err := exec.CommandContext(ctx, args[0], args[1:]...).Output()
-					return string(output), err
-				}
+				cfg.payload = getExecCmdStringFactory(cmd)
 			}
 		}
 
@@ -152,5 +101,61 @@ func setupMqtt(c *caddy.Controller) error {
 		plg.next = next
 		return plg
 	})
+	return nil
+}
+
+func getStringFactory(s string) msgFactory {
+	return func(ctx context.Context, req, res *dhcpv4.DHCPv4) (string, error) {
+		rep := replacer.NewReplacer(ctx, req)
+		return rep.Replace(s), nil
+	}
+}
+
+func getExecCmdStringFactory(cmd []string) msgFactory {
+	return func(ctx context.Context, req, res *dhcpv4.DHCPv4) (string, error) {
+		args := make([]string, len(cmd))
+		rep := replacer.NewReplacer(ctx, req)
+
+		for i, c := range cmd {
+			args[i] = rep.Replace(c)
+		}
+
+		output, err := exec.CommandContext(ctx, args[0], args[1:]...).Output()
+		return string(output), err
+	}
+}
+
+func parseConnectionSettings(cfg *mqttConfig, c *caddy.Controller) error {
+	if cfg.conn == nil {
+		cfg.conn = &mqttConnConfig{}
+	}
+
+	action := c.Val()
+	if action == "clean-session" {
+		cfg.conn.cleanSession = true
+		return nil
+	}
+
+	if !c.NextArg() {
+		return c.ArgErr()
+	}
+
+	switch action {
+	case "name":
+		cfg.name = c.Val()
+	case "broker":
+		cfg.conn.broker = append([]string{c.Val()}, c.RemainingArgs()...)
+	case "user":
+		cfg.conn.user = c.Val()
+	case "password":
+		cfg.conn.password = c.Val()
+	case "qos":
+		i, err := strconv.Atoi(c.Val())
+		if err != nil || i < 0 || i > 2 {
+			return c.SyntaxErr("expected a number between 0 and 2")
+		}
+		cfg.conn.qos = i
+	}
+
 	return nil
 }
