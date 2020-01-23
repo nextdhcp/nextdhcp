9a28ef2bf2df53d25e9e7bc91c2efa2ebc1cd242
Patrick Pacher <patrick.pacher@gmail.com>
2020-01-23 19:47:20 +0100

    chore: added testcases for core/matcher

--- core/matcher/matcher_test.go
+++ core/matcher/matcher_test.go
@@ -2,11 +2,14 @@ package matcher
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"net"
 	"testing"
 
 	"github.com/caddyserver/caddy"
 	"github.com/caddyserver/caddy/caddyfile"
+	"github.com/insomniacslk/dhcp/dhcpv4"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -44,6 +47,77 @@ func TestSetupMatcher(t *testing.T) {
 		} else {
 			assert.NoError(t, err, testCase)
 			assert.NotNil(t, m, testCase)
+		}
+	}
+}
+
+func TestMatcherWithFunctions(t *testing.T) {
+	input := "testFunc(\"arg1\")"
+	disp := caddyfile.NewDispenser("test", bytes.NewBufferString(input))
+
+	fns := map[string]ExprFunc{
+		"testFunc": func(args ...interface{}) (interface{}, error) {
+			assert.Equal(t, "arg1", args[0])
+
+			return true, nil
+		},
+	}
+
+	m, err := SetupMatcherRemainingArgs(&caddy.Controller{Dispenser: disp}, fns)
+	assert.NoError(t, err)
+	assert.NotNil(t, m)
+}
+
+func TestMatch(t *testing.T) {
+	ctx := context.Background()
+	req, _ := dhcpv4.NewDiscovery(net.HardwareAddr{0x00, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff})
+
+	cases := []struct {
+		I string
+		R bool
+		E bool
+	}{
+		{
+			I: "true",
+			R: true,
+			E: false,
+		},
+		{
+			I: "false",
+			R: false,
+			E: false,
+		},
+		{
+			I: "1 == 2",
+			R: false,
+			E: false,
+		},
+		{
+			I: "1 == 1",
+			R: true,
+			E: false,
+		},
+		{
+			I: "msgtype == 'DISCOVER'",
+			R: true,
+		},
+		{
+			I: "msgtype == 'REQUEST'",
+			R: false,
+		},
+	}
+
+	for i, c := range cases {
+		disp := caddyfile.NewDispenser("test", bytes.NewBufferString(c.I))
+		m, err := SetupMatcherRemainingArgs(&caddy.Controller{Dispenser: disp})
+		assert.NoError(t, err)
+
+		res, err := m.Match(ctx, req)
+		if c.E == false {
+			assert.Equal(t, c.R, res, "case %d: %s", i, c.I)
+			assert.NoError(t, err, "case %d: %s", i, c.I)
+		} else {
+			assert.Error(t, err, "case %d: %s", i, c.I)
 		}
 	}
 }
