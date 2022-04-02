0933db054f5829ceb88e64825e2084f5dae5705e
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2022-04-02 02:05:13 -0400

    More fixing

--- plugin/lua/option.go
+++ plugin/lua/option.go
@@ -239,7 +239,7 @@ var optionTypes = map[dhcpv4.OptionCode]*KnownType{
 func GetBuiltinOptionTypes() map[dhcpv4.OptionCode]*KnownType {
 	m := make(map[dhcpv4.OptionCode]*KnownType)
 	for key, value := range optionTypes {
-		currValue = *value
+		currValue := *value
 		m[key] = &currValue
 	}
 
